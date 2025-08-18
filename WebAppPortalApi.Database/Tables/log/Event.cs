1fb28f124dc04f922f89ed1963fd208ee6ba5fb2
Wessel-Boshoff <wesbos337@gmail.com>
2025-08-18 16:31:23 +0200

    ignoring events

---
+++ WebAppPortalApi.Database/Tables/log/Event.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+
+namespace WebAppPortalApi.Database.Tables.log
+{
+    [Table("Events", Schema = "log")]
+    public class Event
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(80)")]
+        public string? LogLevel { get; set; } 
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string? State { get; set; } 
+        [Column( TypeName = "varchar(max)")]
+        public string? StackTrace { get; set; } 
+        [Column(TypeName = "varchar(max)")]
+        public string? Exception { get; set; } 
+        [Required]
+        public DateTime TimeStamp { get; set; }
+    }
+}
