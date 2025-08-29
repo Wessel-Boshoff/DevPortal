edc3ab6240302f1d08e9213cca2a9026f5ecdb05
Wessel-Boshoff <wesbos337@gmail.com>
2025-08-29 08:32:07 +0200

    added sort

--- SoftwareFundamentalsToolkit.Sort/Program.cs
+++ SoftwareFundamentalsToolkit.Sort/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter the list of characters you need to sort:");
+string unsorted = Console.ReadLine() ?? "";
+List<char> sorted = [.. unsorted.ToCharArray()];
+
+bool isUnsorted = true;
+do
+{
+    isUnsorted = false;
+    for (int i = 0; i < sorted.Count - 1; i++)
+    {
+        if (sorted[i] > sorted[i + 1])
+        {
+            // swap
+            char temp = sorted[i];
+            sorted[i] = sorted[i + 1];
+            sorted[i + 1] = temp;
+            isUnsorted = true;
+        }
+    }
+}
+while (isUnsorted);
+
+Console.WriteLine("Sorted characters:");
+Console.WriteLine(new string([.. sorted]));
+
+Console.ReadKey();
